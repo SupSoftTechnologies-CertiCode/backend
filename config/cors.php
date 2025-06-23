74e4368bdd5104c99cadd3380a2dd90058341b08
Aster Benedict Mangabat <asterbenedictmangabat@gmail.com>
2025-06-23 21:32:41 +0800

    configure cors to allow backend

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL')],
+    'allowed_origins' => [env('FRONTEND_URL'), 'https://backend.certicode.tech/sanctum/csrf-cookie'],
 
     'allowed_origins_patterns' => [],
 
